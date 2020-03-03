238dc3e148778ce324949273a627f5e78b1d8afd
Jake Garcia <bballwiz4@gmail.com>
2020-03-03 16:37:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,12 +19,6 @@ class Transfer
 
   def execute_transaction
     if self.valid?
-      self.receiver.deposit(self.amount)
-      self.sender.balance -= self.amount
-      self.status = "complete"
-    else
-      self.status = "rejected"
-      return "Transaction rejected. Please check your account balance."
     end
   end
 
